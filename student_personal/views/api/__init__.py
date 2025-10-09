73db41518678dc614d8cd7d41c0810f2a425e4cb
Craig M. Stimmel <cstimmel@uw.edu>
2025-10-09 14:06:15 -0700

    Fake json representing contacts

--- student_personal/views/api/__init__.py
+++ student_personal/views/api/__init__.py
@@ -12,19 +12,18 @@ class RESTDispatch(View):
     @staticmethod
     def json_response(content={}, status=200):
         try:
-            data = json.dumps(content,
-                              sort_keys=True,
-                              cls=DjangoJSONEncoder)
-            return HttpResponse(data,
-                                status=status,
-                                content_type='application/json')
+            data = json.dumps(content, sort_keys=True, cls=DjangoJSONEncoder)
+            return HttpResponse(
+                data, status=status, content_type="application/json"
+            )
         except TypeError:
             return RESTDispatch().error_response(400)
 
     @staticmethod
-    def error_response(status, message='', content={}):
-        content['error'] = str(message)
-        return HttpResponse(json.dumps(content),
-                            status=status,
-                            content_type='application/json',
-                            )
+    def error_response(status, message="", content={}):
+        content["error"] = str(message)
+        return HttpResponse(
+            json.dumps(content),
+            status=status,
+            content_type="application/json",
+        )
--- student_personal/views/api/econtact.py
+++ student_personal/views/api/econtact.py
@@ -1,13 +1,28 @@
 from student_personal.views.api import RESTDispatch
 from django.core.exceptions import ObjectDoesNotExist
-#from django.utils.decorators import method_decorator
+
+# from django.utils.decorators import method_decorator
 
 
 class EmergencyContact(RESTDispatch):
     def get(self, request):
         try:
-            placeholderjson = {'json': 'placeholder'}
-            return self.json_response(placeholderjson)
+            ecjson = [
+                {
+                    "name": "John Doe",
+                    "phone": "1234567890",
+                    "email": "e1@gmail.com",
+                    "relationship": "",
+                    "last_updated": "",
+                },
+                {
+                    "name": "Jane Doe",
+                    "phone": "5555555555",
+                    "email": "janedoe@gmail.com",
+                    "relationship": "",
+                    "last_updated": "",
+                },
+            ]
+            return self.json_response(ecjson)
         except ObjectDoesNotExist as ex:
-            return self.error_response(404,
-                                       "Contact not found")
+            return self.error_response(404, "Contact not found")
