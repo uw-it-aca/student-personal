368187006a93d0b39709c97514909866e443cde7
Charlon Palacay <charlon@gmail.com>
2025-12-11 11:32:08 -0800

    Fixes frontend tests.

--- student_personal_vue/components/emergency/__tests__/contact-edit.spec.js
+++ student_personal_vue/components/emergency/__tests__/contact-edit.spec.js
@@ -14,9 +14,11 @@ import {
   BModal,
   createBootstrap,
 } from "bootstrap-vue-next";
-import { describe, expect, it } from "vitest";
+import { createPinia } from "pinia";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 
 import ContactEdit from "@/components/emergency/contact-edit.vue";
+import { useEmergencyContactStore } from "@/stores/emergency-contact.js";
 
 // Mock the SCountryCode component
 const SCountryCode = {
@@ -25,41 +27,67 @@ const SCountryCode = {
 };
 
 describe("ContactModal", () => {
-  const wrapper = mount(ContactEdit, {
-    props: {
-      modalData: [
-        {
-          name: "John Doe",
-          phoneNumber: "+442079460958",
-          email: "john.doe@example.com",
-          relationship: "PARENT",
-        },
-        {
-          name: "Jane Doe",
-          phoneNumber: "+912212345678",
-          email: "jane.doe@example.com",
-          relationship: "GUARDIAN",
-        },
-      ],
-    },
-    global: {
-      plugins: [createBootstrap()],
-      components: {
-        BForm,
-        BFormGroup,
-        BInputGroup,
-        BInputGroupText,
-        BFormInput,
-        BFormInvalidFeedback,
-        BFormText,
-        BFormSelect,
-        BFormSelectOption,
-        BFormCheckbox,
-        BButton,
-        BModal,
-        SCountryCode,
+  let wrapper;
+  let pinia;
+
+  beforeEach(() => {
+    // Mock the django context
+    const context = { student: { reg_id: "12345" } };
+    const script = document.createElement("script");
+    script.id = "django-context-data";
+    script.type = "application/json";
+    script.textContent = JSON.stringify(context);
+    document.body.appendChild(script);
+
+    pinia = createPinia();
+    const emergencyContactStore = useEmergencyContactStore(pinia);
+    emergencyContactStore.contacts = [
+      {
+        name: "John Doe",
+        phoneNumber: "+442079460958",
+        email: "john.doe@example.com",
+        relationship: "PARENT",
       },
-    },
+      {
+        name: "Jane Doe",
+        phoneNumber: "+912212345678",
+        email: "jane.doe@example.com",
+        relationship: "GUARDIAN",
+      },
+    ];
+
+    wrapper = mount(ContactEdit, {
+      props: {
+        isPrimary: true,
+      },
+      global: {
+        plugins: [createBootstrap(), pinia],
+        components: {
+          BForm,
+          BFormGroup,
+          BInputGroup,
+          BInputGroupText,
+          BFormInput,
+          BFormInvalidFeedback,
+          BFormText,
+          BFormSelect,
+          BFormSelectOption,
+          BFormCheckbox,
+          BButton,
+          BModal,
+          SCountryCode,
+        },
+      },
+    });
+  });
+
+  afterEach(() => {
+    // cleanup
+
+    const script = document.getElementById("django-context-data");
+    if (script) {
+      document.body.removeChild(script);
+    }
   });
 
   it("hides the modal by default", () => {
