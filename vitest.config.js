765c7dc4e27eb404748342acad6d7c92c6af525b
Charlon Palacay <charlon@gmail.com>
2026-01-06 14:53:16 -0800

    Additional cleanup.

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import Vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [Vue()],
