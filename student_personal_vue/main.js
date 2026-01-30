694ceb60ca80b6369d10e5fe94817b50b9d21e80
Charlon Palacay <charlon@gmail.com>
2026-01-30 08:30:52 -0800

    Remove unused libs.

--- student_personal_vue/main.js
+++ student_personal_vue/main.js
@@ -1,8 +1,6 @@
 import { createBootstrap } from "bootstrap-vue-next";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
-// import VueGtag from "vue-gtag-next";
-import { Vue3Mq } from "vue3-mq";
 
 import App from "@/app.vue";
 import router from "@/router";
@@ -32,11 +30,6 @@ app.config.globalProperties.gettext = window.gettext;
 app.config.globalProperties.ngettext = window.ngettext;
 app.config.globalProperties.interpolate = window.interpolate;
 
-// vue-mq (media queries)
-app.use(Vue3Mq, {
-  preset: "bootstrap5",
-});
-
 // pinia (vuex) state management
 const pinia = createPinia();
 app.use(pinia);
@@ -49,23 +42,6 @@ app.use(createBootstrap());
 
 // vue-router
 app.use(router);
-
-/*
-// vue-gtag-next
-// TODO: un-commment to use Google Analytics for you app. also
-// configure trackRouter located in the router/index.js file
-//
-app.use(VueGtag, {
-  isEnabled: contextStore.context.debugMode, == "false",
-  property: {
-    id: contextStore.context.googleAnalyticsKey,
-    params: {
-      anonymize_ip: true,
-      // user_id: 'provideSomeHashedId'
-    },
-  },
-});
-*/
 
 // microsoft clarity
 Clarity.init(contextStore.context.clarityProjectId);
